cd231fd81a53f8d52b32e82aafffbed3b3a652c9
Hanna Littlefield <hannamlittlefield@gmail.com>
2020-12-18 11:31:56 -0500

    initializng project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './QuoteBox';
+import QuoteBox from './QuoteBox';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <QuoteBox/>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/QuoteBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function QuoteBox() {
+    return (
+    <div id="quote-box">
+    Quote will go here.
+    </div>
+    );
+}
