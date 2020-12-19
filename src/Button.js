b32974b4f5379b60953126bfc26a7a221741c2e4
Hanna Littlefield <hannamlittlefield@gmail.com>
2020-12-19 10:37:30 -0500

    initial commit of project

---
+++ src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Button(){
+    return(
+        <button type="button" class="btn btn-primary" id="new-quote">New quote!</button>
+    );
+}
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Quote from './Quote'
+import Button from './Button'
+import Author from './Author'
 
 export default function QuoteBox() {
     return (
     <div id="quote-box">
-    Quote will go here.
+        <Quote/>
+        <Button/>
+        <Author/>
     </div>
     );
 }
